e65f09e10578fd886ee24c3fc9c0359377a19840
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-02-26 14:20:37 +0100

    Upgrade spring test to Spring Boot 3.2.3
    - Upgrade to Spring Boot 3.2.3
    - Upgrade to Java 21 with Liberica JDK
    - Make Message a top level class
    - Properly exclude autoconfigurations based on profiles

--- frameworks/Java/spring/src/main/java/hello/App.java
+++ frameworks/Java/spring/src/main/java/hello/App.java
@@ -2,17 +2,15 @@ package hello;
 
 import javax.sql.DataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
-import com.zaxxer.hikari.HikariDataSource;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class})
+@SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
